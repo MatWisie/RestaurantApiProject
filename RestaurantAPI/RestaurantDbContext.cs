49eca213f08ca2e592e16da596adc666c0e56f56
MatWisie <Mateuszwisiecki@gmail.com>
2024-02-24 14:24:25 +0100

    Add project files.

---
+++ RestaurantAPI/RestaurantDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantAPI
+{
+    public class RestaurantDbContext : IdentityDbContext<IdentityUser>
+    {
+        public RestaurantDbContext(DbContextOptions<RestaurantDbContext> options)
+    : base(options)
+        {
+        }
+    }
+}
