04e9cd24d642267ffc9ce361a5f23bf72034c659
MatWisie <Mateuszwisiecki@gmail.com>
2024-02-25 19:18:38 +0100

    Added CreateDefaultAdmin method to AuthController

--- RestaurantAPI/Controllers/AuthController.cs
+++ RestaurantAPI/Controllers/AuthController.cs
@@ -20,6 +20,7 @@ namespace RestaurantAPI.Controllers
             _userManager = userManager;
             _roleManager = roleManager;
             _configuration = configuration;
+            CreateDefaultAdmin().Wait();
         }
         public IActionResult Index()
         {
@@ -95,55 +96,64 @@ namespace RestaurantAPI.Controllers
             }
             return Unauthorized();
         }
+
+        [HttpPost]
+        [Route("register-user")]
+        public async Task<IActionResult> RegisterUser([FromBody] RegisterModel model)
         {
             var userExists = await _userManager.FindByNameAsync(model.Username);
             if (userExists != null)
                 return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists!" });
 
-            IdentityUser user = new()
+            IdentityUserModel user = new()
             {
                 Email = model.Email,
                 SecurityStamp = Guid.NewGuid().ToString(),
-                UserName = model.Username
+                UserName = model.Username,
+                FirstTimeLoggedIn = false
             };
             var result = await _userManager.CreateAsync(user, model.Password);
             if (!result.Succeeded)
                 return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." });
+
+            if (!await _roleManager.RoleExistsAsync(UserRoles.User))
+                await _roleManager.CreateAsync(new IdentityRole(UserRoles.User));
+
+            if (await _roleManager.RoleExistsAsync(UserRoles.User))
+            {
+                await _userManager.AddToRoleAsync(user, UserRoles.User);
+            }
 
             return Ok(new Response { Status = "Success", Message = "User created successfully!" });
         }
 
         [HttpPost]
-        [Route("register-admin")]
-        public async Task<IActionResult> RegisterAdmin([FromBody] RegisterModel model)
+        [Route("register-worker")]
+        public async Task<IActionResult> RegisterWorker([FromBody] RegisterModel model)
         {
             var userExists = await _userManager.FindByNameAsync(model.Username);
             if (userExists != null)
                 return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists!" });
 
-            IdentityUser user = new()
+            IdentityUserModel user = new()
             {
                 Email = model.Email,
                 SecurityStamp = Guid.NewGuid().ToString(),
-                UserName = model.Username
+                UserName = model.Username,
+                FirstTimeLoggedIn = true
             };
             var result = await _userManager.CreateAsync(user, model.Password);
             if (!result.Succeeded)
                 return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." });
 
-            if (!await _roleManager.RoleExistsAsync(UserRoles.Admin))
-                await _roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
-            if (!await _roleManager.RoleExistsAsync(UserRoles.User))
-                await _roleManager.CreateAsync(new IdentityRole(UserRoles.User));
+            if (!await _roleManager.RoleExistsAsync(UserRoles.Worker))
+                await _roleManager.CreateAsync(new IdentityRole(UserRoles.Worker));
 
-            if (await _roleManager.RoleExistsAsync(UserRoles.Admin))
+            if (await _roleManager.RoleExistsAsync(UserRoles.Worker))
             {
-                await _userManager.AddToRoleAsync(user, UserRoles.Admin);
+                await _userManager.AddToRoleAsync(user, UserRoles.Worker);
             }
-            if (await _roleManager.RoleExistsAsync(UserRoles.Admin))
-            {
-                await _userManager.AddToRoleAsync(user, UserRoles.User);
-            }
+
             return Ok(new Response { Status = "Success", Message = "User created successfully!" });
         }
 
@@ -168,6 +178,33 @@ namespace RestaurantAPI.Controllers
                 );
 
             return token;
+        }
+
+        private async Task CreateDefaultAdmin()
+        {
+            var existingAdmin = await _userManager.GetUsersInRoleAsync(UserRoles.Admin);
+            if (existingAdmin.Count >= 1)
+            {
+                return;
+            }
+
+            IdentityUserModel user = new()
+            {
+                Email = "defaultAdmin@admin.com",
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = "Admin"
+            };
+            var result = await _userManager.CreateAsync(user, "Admin123!");
+            if (!result.Succeeded)
+                return;
+
+            if (!await _roleManager.RoleExistsAsync(UserRoles.Admin))
+                await _roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
+
+            if (await _roleManager.RoleExistsAsync(UserRoles.Admin))
+            {
+                await _userManager.AddToRoleAsync(user, UserRoles.Admin);
+            }
         }
     }
 }
