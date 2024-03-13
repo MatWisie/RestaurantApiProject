e2a9a9b870ae264f83d504b15316cc54435c3b7f
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-13 18:14:00 +0100

    Added UserController

---
+++ RestaurantAPI/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Models;
+using RestaurantAPI.Models.DTOs;
+using System.Security.Claims;
+using System.Text.RegularExpressions;
+
+namespace RestaurantAPI.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUserModel> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly RestaurantDbContext _context;
+        public UserController(UserManager<IdentityUserModel> userManager, RestaurantDbContext context, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _context = context;
+            _roleManager = roleManager;
+        }
+
+    }
+}
