08ca2e5353c89ce1479f7071c9c04572021a18fc
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-23 14:01:19 +0100

    Added TableGetModel

---
+++ RestaurantAPI/Models/DTOs/TableGetModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantAPI.Models.DTOs
+{
+    public class TableGetModel
+    {
+        public int Id { get; set; }
+        public bool IsAvailable { get; set; } = true;
+        [Required]
+        public int NumberOfSeats { get; set; }
+        [Required]
+        public int GridRow { get; set; }
+        [Required]
+        public int GridColumn { get; set; }
+    }
+}
