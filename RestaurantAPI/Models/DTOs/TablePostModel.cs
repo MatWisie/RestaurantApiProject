56ac5dbeab0ba628f7020f89a417ef726db3936f
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-23 14:01:10 +0100

    Added TablePostModel

---
+++ RestaurantAPI/Models/DTOs/TablePostModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantAPI.Models.DTOs
+{
+    public class TablePostModel
+    {
+        public bool IsAvailable { get; set; } = true;
+        [Required]
+        public int NumberOfSeats { get; set; }
+        [Required]
+        public int GridRow { get; set; }
+        [Required]
+        public int GridColumn { get; set; }
+    }
+}
