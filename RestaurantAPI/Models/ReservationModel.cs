58304cda2ff2c8a741b09416c782800e5d67c201
MatWisie <Mateuszwisiecki@gmail.com>
2024-04-21 17:30:55 +0200

    Changed userid to string in ReservationModel

--- RestaurantAPI/Models/ReservationModel.cs
+++ RestaurantAPI/Models/ReservationModel.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public DateTime From { get; set; }
         public DateTime To { get; set; }
-        public int IdentityUserId { get; set; }
+        public string IdentityUserId { get; set; }
         public virtual IdentityUserModel IdentityUserModel { get; set; }
         public int TableModelId { get; set; }
         public virtual TableModel TableModel { get; set; }
