66955ea39c354dc764a143b0801902cfdeeb18a7
MatWisie <Mateuszwisiecki@gmail.com>
2024-04-21 17:30:07 +0200

    Added GetUser to UserController

--- RestaurantAPI/Controllers/UserController.cs
+++ RestaurantAPI/Controllers/UserController.cs
@@ -49,6 +49,32 @@ namespace RestaurantAPI.Controllers
         }
 
         [Authorize]
+        [HttpGet("get-user/{id}")]
+        public async Task<IActionResult> GetUser(string id)
+        {
+            try
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(e => e.Id == id);
+                if (user == null)
+                    return NotFound("User not found");
+
+                var userRole = await _userManager.GetRolesAsync(user);
+                UserGetModel userGetModels = new UserGetModel()
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Role = userRole.Count > 0 ? userRole[0] : "No Role Assigned"
+                };
+
+                return Ok(userGetModels);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Authorize]
         [HttpPost("edit-user")]
         public async Task<IActionResult> EditUser([FromBody] UserEditModel userEditModel)
         {
@@ -68,6 +94,49 @@ namespace RestaurantAPI.Controllers
                 user.UserName = userEditModel.Username;
                 await _userManager.ChangePasswordAsync(user, user.PasswordHash, userEditModel.Password);
                 user.Age = userEditModel.Age;
+
+                var result = await _userManager.UpdateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return Ok("User updated successfully");
+                }
+                else
+                {
+                    return BadRequest(result.Errors);
+                }
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+
+        [Authorize]
+        [HttpPost("change-user-password")]
+        public async Task<IActionResult> EditUserPassword([FromBody] UserChangePasswordModel userNewPasswordModel)
+        {
+            if (User.Claims.FirstOrDefault(e => e.Type == ClaimTypes.NameIdentifier).Value == userNewPasswordModel.UserId || User.IsInRole(UserRoles.Admin))
+            {
+                var user = await _userManager.FindByIdAsync(userNewPasswordModel.UserId);
+                if (user == null)
+                {
+                    return NotFound("User not found");
+                }
+
+                var samePassword = await _userManager.CheckPasswordAsync(user, userNewPasswordModel.Password);
+                if (!samePassword)
+                {
+                    return BadRequest("Current password is incorrect");
+                }
+
+                var passwordPattern = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$");
+
+                if (!passwordPattern.IsMatch(userNewPasswordModel.Password))
+                    return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one digit, and one special character." });
+
+                await _userManager.ChangePasswordAsync(user, user.PasswordHash, userNewPasswordModel.NewPassword);
+                user.FirstTimeLoggedIn = false;
 
                 var result = await _userManager.UpdateAsync(user);
 
