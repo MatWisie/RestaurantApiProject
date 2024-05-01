81ba824e1017d08f8cff9792b1bd5af81b02120e
MatWisie <Mateuszwisiecki@gmail.com>
2024-05-01 19:45:19 +0200

    Added LoginLogModel

---
+++ RestaurantAPI/Models/LoginLogModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace RestaurantAPI.Models
+{
+    public class LoginLogModel
+    {
+        public int Id { get; set; }
+        public string IpAddress { get; set; }
+        public bool Success { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public HttpStatusCode StatusCode { get; set; }
+        public string? IdentityUserId { get; set; }
+        public virtual IdentityUserModel? IdentityUserModel { get; set; }
+    }
+}
