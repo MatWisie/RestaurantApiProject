ecde0c0d4f3a87d64aea65e95eaa62219242f8c3
MatWisie <Mateuszwisiecki@gmail.com>
2024-05-02 21:03:51 +0200

    Added ReservationPrivateGetModel

---
+++ RestaurantAPI/Models/ReservationPrivateGetModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantAPI.Models
+{
+    public class ReservationPrivateGetModel
+    {
+        public int Id { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        public int TableModelId { get; set; }
+    }
+}
